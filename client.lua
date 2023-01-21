053cc22911b78ff34eb09d570a0a53848364aeb1
ChernyyOrel <92865037+ChernyyWorks@users.noreply.github.com>
2023-01-21 22:24:08 +0100

    V0.5
    Unfinished, it needs a banner.

---
+++ client.lua
@@ -0,0 +1,54 @@
+function notification(text)
+    BeginTextCommandThefeedPost('STRING')
+    AddTextComponentSubstringPlayerName(text)
+    EndTextCommandThefeedPostTicker(0,1)
+end
+
+local RockstarEditorMenu = RageUI.CreateMenu("", "Options", nil, nil, "rageui", "banner_editor") --"TeapotBanner"
+RockstarEditorMenu.EnableMouse = false;
+local EditorRecording = false
+
+function RageUI.PoolMenus:MenuRockstarEditor()
+    RockstarEditorMenu:IsVisible(function(Items)
+        if EditorRecording then
+            Items:AddButton("Arrêter et ~g~sauvegarder~s~", nil, { IsDisabled = false, RightLabel = "" }, function(onSelected)
+                if (onSelected) then
+                    StopRecordingAndSaveClip()
+                    EditorRecording = not(EditorRecording)
+                    Items:CloseAllMenu()
+                end
+            end)
+            Items:AddButton("Arrêter et ~r~supprimer~s~", nil, { IsDisabled = false, RightLabel = "" }, function(onSelected)
+                if (onSelected) then
+                    StopRecordingAndDiscardClip()
+                    notification("~p~Editeur~s~\nL'enregistrement à bien été ~r~supprimé~s~ !")
+                    EditorRecording = not(EditorRecording)
+                    Items:CloseAllMenu()
+                end
+            end)
+        elseif EditorRecording == false then
+            Items:AddButton("Commencer l'enregistrement", nil, { IsDisabled = false, RightLabel = "→" }, function(onSelected)
+                if (onSelected) then
+                    EditorRecording = not(EditorRecording)
+                    StartRecording(1)
+                    Items:CloseAllMenu()
+                end
+            end)
+            Items:AddLine()
+            Items:AddButton("Editeur", "Voir et éditer les enregistrements sauvegardés !", { IsDisabled = false, RightLabel = "" }, function(onSelected)
+                if (onSelected) then
+                    NetworkSessionLeaveSinglePlayer()
+                    ActivateRockstarEditor()
+                    Items:CloseAllMenu()
+                end
+            end)
+        end
+    end, function()
+    end)
+end
+
+RegisterCommand("editor",function()
+    if not(IsEntityDead(PlayerPedId())) then
+        RageUI.Visible(RockstarEditorMenu, not RageUI.Visible(RockstarEditorMenu))
+    end
+end)
---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'adamant'
+game 'gta5'
+lua54 'yes'
+
+client_script {
+    -- RageUIx | https://github.com/Florian-1801/RageUIx
+    'src/RageUI.lua',
+    'src/Menu.lua',
+    'src/MenuController.lua',
+    'src/components/*.lua',
+    'src/elements/*.lua',
+    'src/items/*.lua',
+    --
+    "client.lua",
+}
